37402919a2e2a5cb27da775d780a9520943edbfe
Gokul-pga <118168471+Gokul-pga@users.noreply.github.com>
2023-11-14 20:22:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-// alert("Hey Hiii");
+alert("Hey Hiii");
 
-// let mybutton = document.getElementById("mybtn");
+let mybutton = document.getElementById("mybtn");
 
-// window.onscroll = function()
-//     {scrollfunction()};
+window.onscroll = function()
+    {scrollfunction()};
 
-// function scrollfunction(){
-//     if (document.body.scrollTop > 20 == document.documentElement.scrollTop > 20 ){
-//         mybutton.style.display = "none";
-//     }
-//     else {
-//         mybutton.style.display = "block";
-//     }
-// }
+function scrollfunction(){
+    if (document.body.scrollTop > 20 == document.documentElement.scrollTop > 20 ){
+        mybutton.style.display = "none";
+    }
+    else {
+        mybutton.style.display = "block";
+    }
+}
 
-// function topfunction() {
-//     document.body.scrollTop = 0;
-//     document.documentElement.scrollTop = 0;
-// }
+function topfunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
